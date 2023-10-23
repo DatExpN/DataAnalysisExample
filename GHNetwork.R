3cad76f98d95841ac6c56a5d5c947e5a7bc51509
DatExpN <148682419+DatExpN@users.noreply.github.com>
2023-10-23 16:35:10 +0300

    Update GHNetwork.R

--- GHNetwork.R
+++ GHNetwork.R
@@ -1,5 +1,6 @@
 # Предобработка данных.
-# Для создания графа мы будем использовать только информацию про актеров - переменные star1, Star2, Star3 и Star4. Строим граф, где вершинами будут актеры, а связью - факт участия в съемках одних и тех же фильмов.
+# Для создания графа мы будем использовать только информацию про актеров - переменные star1, Star2, Star3 и Star4. 
+# Строим граф, где вершинами будут актеры, а связью - факт участия в съемках одних и тех же фильмов.
 library(readr)
 imdb_top_1000 <- read_csv("C:/Users/Nadezhda/Desktop/imdb_top_1000.csv")
 df <- imdb_top_1000[, 11: 14]
@@ -10,7 +11,8 @@ relations3 <- data.frame(from = df[, 1], to = df[, 4])
 relations4 <- data.frame(from = df[, 2], to = df[, 3])
 relations5 <- data.frame(from = df[, 2], to = df[, 4])
 relations6 <- data.frame(from = df[, 3], to = df[, 4])
-listdata <- lapply(list(relations1, relations2, relations3, relations4, relations5, relations6), function(x) {setNames(x, c('Star1', 'Star2'))})
+listdata <- lapply(list(relations1, relations2, relations3, relations4, relations5, relations6), 
+                   function(x) {setNames(x, c('Star1', 'Star2'))})
 gmain <- do.call(rbind, listdata)
 g1 <- graph_from_data_frame(gmain, directed=FALSE)
 summary(g1)
@@ -25,10 +27,12 @@ g2 <- set_vertex_attr(g2, 'degree', v = degree_graph)
 coreness <- core_periphery(g2, method = "rk1_dc")$vec # определяем позицию вершины относительно ядра/периферии
 g2 <- set_vertex_attr(g2, 'core', v = coreness)
 summary(g2)
-# Визуализируем полученный граф с учетом информации о числе контактов и позиции относительно ядра/периферии. Красным цветом обозначены вершины, попавшие в ядро, размер выделяет вершины с числом контактов больше 20.
+# Визуализируем полученный граф с учетом информации о числе контактов и позиции относительно ядра/периферии. 
+# Красным цветом обозначены вершины, попавшие в ядро, размер выделяет вершины с числом контактов больше 20.
 plot(g2, edge.curved = 0.2, vertex.color= ifelse(V(g2)$core > 0, 'red', 'grey'), vertex.size = ifelse(V(g2)$degree > 20, 6, 2.5), layout=layout_with_fr, vertex.label = NA)
 # https://github.com/DatExpN/DataAnalysisExample/blob/main/RPlot9.png
-# Посмотрим, какие актеры имеют наибольшее число контактов в индустрии (контакты основаны на числе фильмов, в которых они снимались вместе с другими актерами).
+# Посмотрим, какие актеры имеют наибольшее число контактов в индустрии 
+# (контакты основаны на числе фильмов, в которых они снимались вместе с другими актерами).
 actors <- as.data.frame(cbind(V(g2)$name, V(g2)$degree))
 colnames(actors) <- c('Actor', 'Number_of_contacts')
 actors$Number_of_contacts <- as.numeric(actors$Number_of_contacts)
@@ -38,3 +42,4 @@ library(ggplot2)
 actors2$Actor <- factor(actors2$Actor, levels = actors2$Actor[order(actors2$Number_of_contacts, decreasing = FALSE)])
 ggplot(actors2, aes(x=Actor, y=Number_of_contacts)) + 
 geom_bar(stat = "identity", fill = ifelse(actors2$Number_of_contacts > 20, 'red', 'grey')) + coord_flip()
+# https://github.com/DatExpN/DataAnalysisExample/blob/main/RPlot10.png
