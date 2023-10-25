5c231f8cc74d27a6c5d1506961f788470a8c9ff4
DatExpN <148682419+DatExpN@users.noreply.github.com>
2023-10-25 13:02:42 +0300

    Update GHNetwork.R

--- GHNetwork.R
+++ GHNetwork.R
@@ -1,6 +1,6 @@
 # База данных по 1000 фильмам и их оценкам на IMDB.
 # Предобработка данных.
-# Для создания графа мы будем использовать только информацию про актеров - переменные star1, Star2, Star3 и Star4. 
+# Для создания графа мы будем использовать только информацию про актеров - переменные Star1, Star2, Star3 и Star4. 
 # Строим граф, где вершинами будут актеры, а связью - факт участия в съемках одних и тех же фильмов.
 
 
@@ -19,15 +19,13 @@ listdata <- lapply(list(relations1, relations2, relations3, relations4, relation
 gmain <- do.call(rbind, listdata)
 g1 <- graph_from_data_frame(gmain, directed=FALSE)
 summary(g1)
-library(netUtils)
-
 
 # Так как у нас нет изначально заданных атрибутов, для визуализации сделаем атрибуты при помощи информации о позиции 
 # вершины в сети. Так как мы анализируем сеть актеров, нам может быть интересно какие актеры находятся в ядре индустрии, 
 # а какие на периферии. Также нам может быть интересно, какие актеры имеют наибольшее число контактов в индустрии. 
 # Для того, чтобы рассчитать позицию актера относительно ядра/периферии, нам нужно выделить в сети главный компонент.
 
-
+library(netUtils)
 c <- components(g1, mode = 'weak')
 c$csize
 g2 <- decompose(g1, mode = 'weak', min.vertices = 1724)
@@ -59,10 +57,12 @@ library(ggplot2)
 actors2$Actor <- factor(actors2$Actor, levels = actors2$Actor[order(actors2$Number_of_contacts, decreasing = FALSE)])
 ggplot(actors2, aes(x=Actor, y=Number_of_contacts)) + 
 geom_bar(stat = "identity", fill = ifelse(actors2$Number_of_contacts > 20, 'red', 'grey')) + coord_flip()
+# В топ-10 актеров с наибольшим числом контактов входят: Роберт Де Ниро, Том Хэнкс, Аль Пачино, Клинт Иствуд, Брэд Питт,
+# Мэтт Деймон, Леонардо Ди Каприо, Кристиан Бэйл, Джеймс Стюарт, Скарлетт Йоханссон.
 # https://github.com/DatExpN/DataAnalysisExample/blob/main/PopularActors.png
 
 # Посмотрим, на какие сообщества (community) разделится наша сеть - в данном случае сообщество будет объединять актеров, 
-# которые часто снимаются вместе. Попробуем два алгоритма - Louvain и Girvan-Newman.
+# которые часто снимаются вместе. Попробуем два алгоритма - Louvain и Girvan-Newman. 
 set.seed(123)
 love1 <- cluster_louvain(g2) # применяем алгоритм поиска сообществ Louvain
 modularity(love1) # модулярность говорит о том, насколько хорошо сеть разделилась на сообщества
@@ -94,7 +94,8 @@ com2 <- replace(com2, com2 == 12, 4)
 com2 <- replace(com2, com2 == 25, 5)
 g2 <- set_vertex_attr(g2, 'comnew', v = com2)
 colrs <- c('grey','red', 'blue','purple','pink')
-plot(g2, vertex.color= colrs[as.factor(V(g2)$comnew)], edge.curved = 0.2, vertex.size=3.5, vertex.label = NA, edge.color = "grey", layout=layout_with_fr) # визуализируем только большие сообщества (с числом вершин от 70 и выше). 
+plot(g2, vertex.color= colrs[as.factor(V(g2)$comnew)], edge.curved = 0.2, vertex.size=3.5, vertex.label = NA, 
+     edge.color = "grey", layout=layout_with_fr) # визуализируем только большие сообщества (с числом вершин от 70 и выше). 
 # В данном случае берем такие границы деления, потому что этот алгоритм делит граф на большое число небольших сообществ.
 # https://github.com/DatExpN/DataAnalysisExample/blob/main/GraphNewman.png
 
