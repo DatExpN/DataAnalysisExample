ed27d1d1fb8b23bcbb8bdfa8f35b454cd54f9d67
DatExpN <148682419+DatExpN@users.noreply.github.com>
2023-10-26 11:30:05 +0300

    Update GHNetwork.R

--- GHNetwork.R
+++ GHNetwork.R
@@ -52,7 +52,8 @@ actors2 <- actors[1:35, ]
 library(ggplot2)
 actors2$Actor <- factor(actors2$Actor, levels = actors2$Actor[order(actors2$Number_of_contacts, decreasing = FALSE)])
 ggplot(actors2, aes(x=Actor, y=Number_of_contacts)) + 
-geom_bar(stat = "identity", fill = ifelse(actors2$Number_of_contacts > 20, 'red', 'grey')) + coord_flip()
+geom_bar(stat = "identity", fill = ifelse(actors2$Number_of_contacts > 20, 'red', 'grey')) + 
+coord_flip()
 # В топ-10 актеров с наибольшим числом контактов входят: Роберт Де Ниро, Том Хэнкс, Аль Пачино, Клинт Иствуд, Брэд Питт,
 # Мэтт Деймон, Леонардо Ди Каприо, Кристиан Бэйл, Джеймс Стюарт, Скарлетт Йоханссон.
 # https://github.com/DatExpN/DataAnalysisExample/blob/main/PopularActors.png
@@ -64,7 +65,7 @@ love1 <- cluster_louvain(g2) # применяем алгоритм поиска 
 modularity(love1) # модулярность говорит о том, насколько хорошо сеть разделилась на сообщества
 sizes(love1) # смотрим количество и размер полученных сообществ
 plot(g2, vertex.color=membership(love1), edge.curved = 0.2, vertex.size=3.5, vertex.label = NA, 
-     edge.color = "grey", layout=layout_with_fr) # визуализируем сеть с учетом информации о сообществах
+edge.color = "grey", layout=layout_with_fr) # визуализируем сеть с учетом информации о сообществах
 love2 <- ifelse(love1$membership == 5 | love1$membership == 9 | love1$membership == 13 | love1$membership == 16, love1$membership, 0)
 love2 <- replace(love2, love2 == 0, 1)
 love2 <- replace(love2, love2 == 5, 2)
@@ -74,7 +75,7 @@ love2 <- replace(love2, love2 == 16, 5)
 g2 <- set_vertex_attr(g2, 'comluv', v = love2)
 colrs <- c('grey',"orange", "tomato", "green", 'blue')
 plot(g2, vertex.color= colrs[as.factor(V(g2)$comluv)], edge.curved = 0.2, vertex.size=3.5, vertex.label = NA, 
-     edge.color = "grey", layout=layout_with_fr) # визуализируем только большие сообщества (с числом вершин от 100)
+edge.color = "grey", layout=layout_with_fr) # визуализируем только большие сообщества (с числом вершин от 100)
 # https://github.com/DatExpN/DataAnalysisExample/blob/main/GraphLouvain.png
 set.seed(123)
 com1 <- cluster_edge_betweenness(g2) # применяем алгоритм поиска сообществ Girvan-Newman
@@ -91,7 +92,7 @@ com2 <- replace(com2, com2 == 25, 5)
 g2 <- set_vertex_attr(g2, 'comnew', v = com2)
 colrs <- c('grey','red', 'blue','purple','pink')
 plot(g2, vertex.color= colrs[as.factor(V(g2)$comnew)], edge.curved = 0.2, vertex.size=3.5, vertex.label = NA, 
-     edge.color = "grey", layout=layout_with_fr) # визуализируем только большие сообщества (с числом вершин от 70 и выше). 
+edge.color = "grey", layout=layout_with_fr) # визуализируем только большие сообщества (с числом вершин от 70 и выше). 
 # В данном случае берем такие границы деления, потому что этот алгоритм делит граф на большое число небольших по размеру сообществ (если визуализируем только сообщества размером больше 100 вершин, то получим только одну группу).
 # https://github.com/DatExpN/DataAnalysisExample/blob/main/GraphNewman.png
 
@@ -102,6 +103,6 @@ library(GGally)
 netg <- intergraph::asNetwork(g2)
 summary(netg)
 ggnet2(netg, color = 'comluv', palette = c('1' = 'grey67', '2' = 'yellow', '3' = 'limegreen', '4' = 'turquoise1', '5' = 'orangered'), 
-       edge.size = 0.01, mode = 'fruchtermanreingold', size = 2.5) + guides(color = FALSE, size = FALSE) 
+edge.size = 0.01, mode = 'fruchtermanreingold', size = 2.5) + guides(color = FALSE, size = FALSE) 
 # Визуализируем сообщества размером более 100 вершин, полученные после применения алгоритма Louvain.
 # https://github.com/DatExpN/DataAnalysisExample/blob/main/GraphLouvain2.png
